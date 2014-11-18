9fc4cc8568a80b92ed92db734ef60a1fccb55ff6
Finn-Lennart Heemeyer <finn@heemeyer.net>
2014-11-18 14:10:20 +0100

    fixes #1

--- ICanHaz.load.js
+++ ICanHaz.load.js
@@ -1,4 +1,4 @@
-(function(window, document, $) {
+(function($, ich) {
 
   /*
    * ICanHaz.load.js
@@ -16,7 +16,7 @@
    * by Finn-Lennart Heemeyer, License: MIT
    */
 
-  var ich = window.ich,
+  var clearAll = ich.clearAll,
       templates = {};
 
   ich.load = function(templateName, cb, templatePath, templateExtension) {
@@ -32,7 +32,6 @@
                  templateName, '.',
                  (templateExtension || ich.templateExtension || 'mustache')].join('');
 
-
       // Request template file async
       $.get(path, function(data) {
 
@@ -45,7 +44,7 @@
         // Execute all callbacks
         for (var i = 0; i < templates[templateName].callbacks.length; i++) {
           templates[templateName].callbacks[i]();
-        };
+        }
       });
     } else if (templates[templateName].ready) {
       // If template has been loaded before, execute callback
@@ -56,16 +55,23 @@
     }
   };
 
+  /*
+   * Fast version: Do everything in one function
+   */
+  ich.loadTemplate = function(templateName, data, cb, templatePath, templateExtension) {
+    ich.load(templateName, function() {
+      // Compute template from data, execute callback
+      var content = ich[templateName](data);
+      cb(content);
+    }, templatePath, templateExtension);
+  };
 
   /*
    * Make sure to also clean up templates object when calling clearAll()
    */
-  var clearAll = ich.clearAll;
-
   ich.clearAll = function() {
     templates = {};
-
     clearAll();
   };
 
-})(window, window.document, window.jQuery)
+})(window.jQuery, window.ich);
